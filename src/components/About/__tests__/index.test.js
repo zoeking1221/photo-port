4f694eb589bba3d7da84c2779e32468209b90ec9
Zoe King <zoeking1221@gmail.com>
2021-03-21 17:21:37 -0400

    completed testing, ready to deploy

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,22 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-// configure the testing environment
 afterEach(cleanup);
 
-// create baseline test
 describe('About component', () => {
-    // First test - verifies that the component is rendering
-    it('renders', () => {
-        render(<About />);
-    });
+  it('renders', () => {
+    render(<About />);
+  });
 
-    // Second test
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    }) 
-});
+  it('matches snapshot', () => {
+    const { asFragment } = render(<About />);
 
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
