4086728f0be396aae1ee19749e94d0d9726a5a0b
Zoe King <zoeking1221@gmail.com>
2021-03-21 16:15:56 -0400

    contact component

--- src/App.js
+++ src/App.js
@@ -17,18 +17,27 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
@@ -8,13 +8,31 @@ function ContactForm() {
 
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     //  using the setFormState function to update the formState value for the name property
     // assign the value taken from the input field in the UI with e.target.value 
     // and assign this value to the property formState.name
     // **the 'name' property of target actually refers to the NAME attribute of the form element
     // this attribute value matches the property names of formState (name, email, message) and allows us to use [] to create dynamic property names
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                  } else {
+                    setErrorMessage('');
+                  }
+              }
+          }
+          if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
     }
 
     function handleSubmit(e) {
@@ -28,16 +46,21 @@ function ContactForm() {
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+              <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
-              <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+              <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
             </div>
             <div>
               <label htmlFor="message">Message:</label>
-              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+              <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
             </div>
+            {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            )}
             <button type="submit">Submit</button>
           </form>
         </section>
